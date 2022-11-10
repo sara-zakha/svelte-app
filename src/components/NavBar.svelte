c5d41f930561f7e7ccad46108e67f86f7c1c96e5
sara-zakha <zakha.sara@gmail.com>
2022-11-10 15:58:52 +0100

    implement navbar

---
+++ src/components/NavBar.svelte
@@ -0,0 +1,55 @@
+<script>
+    export let header = "";
+    export let navList = [];
+</script>
+
+<section id="nav-bar">
+    <nav class="navbar main-bgcolor navbar-expand-md navbar-dark">
+        <a class="navbar-brand company_brand" href="/">
+            {header}
+        </a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon" />
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                {#each navList as list}
+                    <li class="nav-item">
+                        <a class="nav-link light-color" href={list.url}
+                            >{list.label}</a
+                        >
+                    </li>
+                {/each}
+            </ul>
+        </div>
+    </nav>
+</section>
+
+<style>
+    #nav-bar {
+        position: sticky;
+        top: 0;
+        z-index: 10;
+    }
+    .navbar {
+        padding: 0 20px !important;
+    }
+    .navbar-nav li {
+        padding: 0 0 0 20px;
+    }
+
+    .navbar-nav li a {
+        font-weight: 600;
+        text-transform: uppercase;
+        float: right;
+        text-align: left;
+    }
+</style>
